5a3f28f1a2c77d153356cff11b83ec45a9e3eaec
teox79 <teox1979@gmail.com>
2024-10-20 22:28:10 +0200

    add news

--- src/data/news.ts
+++ src/data/news.ts
@@ -62,11 +62,88 @@ export const newsItemsData: INewsItemsByMonth = {
     "112024": [
         {
             "date": "01/11/2024",
-            "description": "01 Novembre news"
+            "description": "E' Festa giocatrici a riposo !!!! niente allenamenti"
+        },
+        {
+            "date": "04/11/2024",
+            "description": "Ricordiamo allenamenti presso la palestra di Paina dalle 17:00 alle 19:00"
+        },
+        {
+            "date": "07/11/2024",
+            "description": "Ricordiamo allenamenti presso la palestra JEAN MONNET dalle 17:00 alle 19:00"
+        },
+        {
+            "date": "08/11/2024",
+            "description": "Ricordiamo allenamenti presso la palestra JEAN MONNET dalle 17:00 alle 19:00"
+        },
+        {
+            "date": "09/11/2024",
+            "description": "PVI LA RETE DI EMMA ARDOR2K AROSIO VS PRO VICTORIA AZZURRA"
+        },
+        {
+            "date": "10/11/2024",
+            "description": "DERBY !!! PVI LA RETE DI EMMA ARDOR2K AROSIO VS PVI LA RETE DI EMMA ARDOR2K MARIANO"
+        },
+        {
+            "date": "11/11/2024",
+            "description": "Ricordiamo allenamenti presso la palestra di Paina dalle 17:00 alle 19:00"
+        },
+        {
+            "date": "14/11/2024",
+            "description": "Ricordiamo allenamenti presso la palestra JEAN MONNET dalle 17:00 alle 19:00"
         },
         {
             "date": "15/11/2024",
-            "description": "15 Novembre news"
+            "description": "Ricordiamo allenamenti presso la palestra JEAN MONNET dalle 17:00 alle 19:00"
         },
+        {
+            "date": "15/11/2024",
+            "description": "Ricordiamo allenamenti presso la palestra JEAN MONNET dalle 17:00 alle 19:00"
+        },
+        {
+            "date": "16/11/2024",
+            "description": "SAMPIETRINA VOLL. SEVESO VS PVI LA RETE DI EMMA ARDOR2K AROSIO"
+        },
+        {
+            "date": "16/11/2024",
+            "description": "PVI LA RETE DI EMMA ARDOR2K MARIANO VS L'ARCA VOLLEY"
+        },
+        {
+            "date": "21/11/2024",
+            "description": "Ricordiamo allenamenti presso la palestra JEAN MONNET dalle 17:00 alle 19:00"
+        },
+        {
+            "date": "22/11/2024",
+            "description": "Ricordiamo allenamenti presso la palestra JEAN MONNET dalle 17:00 alle 19:00"
+        },
+        {
+            "date": "23/11/2024",
+            "description": "PRO VICTORIA AZZURRA VS PVI LA RETE DI EMMA ARDOR2K AROSIO"
+        },
+        {
+            "date": "23/11/2024",
+            "description": "SAMPIETRINA VOLL. SEVESO VS PVI LA RETE DI EMMA ARDOR2K MARIANO"
+        },
+        {
+            "date": "24/11/2024",
+            "description": "PVI LA RETE DI EMMA ARDOR2K MARIANO VS SAMPIETRINA VOLL. SEVESO"
+        },
+        {
+            "date": "25/11/2024",
+            "description": "Ricordiamo allenamenti presso la palestra di Paina dalle 17:00 alle 19:00"
+        },
+        {
+            "date": "28/11/2024",
+            "description": "Ricordiamo allenamenti presso la palestra JEAN MONNET dalle 17:00 alle 19:00"
+        },
+        {
+            "date": "29/11/2024",
+            "description": "Ricordiamo allenamenti presso la palestra JEAN MONNET dalle 17:00 alle 19:00"
+        },
+        {
+            "date": "30/11/2024",
+            "description": "PVI LA RETE DI EMMA ARDOR2K MARIANO VS PRO VICTORIA AZZURRA"
+        }
+
     ],
 }
